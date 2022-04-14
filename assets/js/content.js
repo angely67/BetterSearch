5df68bebf0336f316703cac6c2be15c5727882fc
angely67 <82392932+angely67@users.noreply.github.com>
2022-04-14 16:07:26 -0400

    add storage

--- assets/js/content.js
+++ assets/js/content.js
@@ -7,31 +7,46 @@ Element.prototype.documentOffsetTop = function () {
 var all = document.querySelectorAll('*')
 var root = all[0]
 var list = root.innerText.split(/\r?\n/);
-list = list.filter(block => block.split(' ').length >= 3);
-console.log("Atlantic Ocean".split(' ').length);
-console.log(list);
+list = list.filter(block => block.split(' ').length >= 2);
 var result_elements = [];
 var index = 0;
 
+chrome.storage.local.get(['loaded', 'all', 'index','url'], 
+  function(items) {      
+        if(items.loaded && document.location.href===items.url){
+            getElements(items.all);
+            highlightResults(items.index);
+        }
+  });
+
 function scrollToElement(element) {
-    console.log(element);
     element.scrollIntoView(); 
     var top = element.documentOffsetTop() - ( window.innerHeight / 2 );
     window.scrollTo( 0, Math.max(top, window.pageYOffset - (window.innerHeight/2))) ;
 }
 
-function highlightResults(results){
-    let copy = [...results];
-    document.querySelectorAll('*').forEach(e => {
-        if(copy.includes(e.innerText)){
-            copy.splice(copy.indexOf(e.innerText), 1);
+function getElements(results){
+    result_elements = [];
+    results.forEach(r => {
+        document.querySelectorAll('*').forEach(e => {
+            if(e.innerText === r){
+                result_elements.push(e);
+            }
+        });
+    })
+    return result_elements;
+}
+function highlightResults(index){
+    result_elements.forEach(e => {
             e.style.background = "yellow";
-            result_elements.push(e);
-        }
     });
-        scrollToElement(result_elements[0]);
+    focusElement(result_elements[index]);
+    scrollToElement(result_elements[index]);
 }
 
+function focusElement(element){
+    element.style.background = "orange"
+}
 function nextElement(){
     index++;
     if(index >= result_elements.length){
@@ -48,7 +63,7 @@ function prevElement(){
     scrollToElement(result_elements[index]);
 }
 
-async function processMessage(msg) {
+async function fetchSemantic(data) {
     var r = await fetch('http://localhost:5000/api/semantic', {
             method: 'POST',
             headers: {
@@ -58,10 +73,10 @@ async function processMessage(msg) {
             body: JSON.stringify(data)
         })
         var result = await r.json();
-            //console.log("done");
-            //console.log(result);
             //results = [list[30], list[50], list[105]];
-        highlightResults(result);
+        getElements(result);
+        highlightResults(0);
+        return result;
             //sendResponse(result);
   }
 
@@ -72,36 +87,32 @@ function gotMessage(request, sender, sendResponse){
             sendResponse();
             return;
         }
-        /*var para = document.getElementsByTagName('p')
-        for (elt of para){
-            elt.style['background-color'] = '#FF00FF'
-        }*/
-        /*if(request.value){
-            let re = new RegExp(request.value,"g"); // search for all instances
-		    let newText = text.replace(re, `<mark>${request.value}</mark>`);
-		    document.body.innerHTML = newText;
-        }*/
-        //using the request.value and list (data) fetch from backend
+        chrome.storage.local.set({
+            'request' : "option1",
+            'value' : request.value,
+            'url' : document.location.href,
+            'loaded' : false}, 
+            function(result) {});
+
+        (async () => {
         result_elements = [];
         index = 0;
 
         let data = {data: list, query: request.value}
         console.log("start");
-        /*fetch('http://localhost:5000/api/semantic', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-                    },
-            body: JSON.stringify(data)
-        }).then(r => r.json())
-        .then(result => {
-            //console.log("done");
-            //console.log(result);
-            //results = [list[30], list[50], list[105]];
-            highlightResults(result);
-            //sendResponse(result);
-        });*/
+        const r = await fetchSemantic(data);
+        let ret = {cur_text:r[0], total:r.length, cur_index:0};
+        console.log(ret);
+        chrome.storage.local.set({
+            'all' : r,
+            'loaded' : true,
+            'total' : ret.total,
+            'index' : 0,
+            'text' : r[0]}, 
+            function(result) {});
+        sendResponse(ret);
+        })();
+        return true; 
     }
     if(request.request === "next"){
         nextElement()
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,20 +1,45 @@
 
-var results = [];
+var results;
 document.addEventListener('DOMContentLoaded', function() {
+  chrome.storage.local.get(['request', 'value', 'loaded', 'total','index','text'], 
+  function(items) {
+    if(items.request === "option1"){
+        let input = document.getElementById('text-opt1');
+        input.value = items.value;
+    }
+    let text = document.getElementById('text');
+    let amount = document.getElementById('amount');
+
+    if(items.loaded){
+      text.innerHTML = items.text.substring(0, 30);
+      amount.classList.remove('hidden');
+      amount.innerHTML = (items.index+1)+" of "+items.total+" results";
+    }
+    else{
+      text.innerHTML = "Loading";
+      amount.classList.add('hidden');
+    }
+  });
   var checkButton = document.getElementById('btn-opt1');
   checkButton.addEventListener('click', function() {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      let text = document.getElementById('text');
+      text.innerHTML = "Loading";
+      let amount = document.getElementById('amount');
+      amount.classList.add('hidden');
+
       let msg = {
         request: "option1",
-        value: document.getElementById('text-opt1').value,
+        value: document.getElementById('text-opt1').value
       }
+
       chrome.tabs.sendMessage(tabs[0].id, msg, function(result){
         results = result;
-      alert(result);
-        /*let resultBox = document.getElementById('result');
-        resultBox.innerHTML = results[0];
+        let text = document.getElementById('text');
+        text.innerHTML = results.cur_text.substring(0, 30);
         let amount = document.getElementById('amount');
-        amount.innerHTML = "0 of "+results.length+"results";*/
+        amount.classList.remove('hidden');
+        amount.innerHTML = (results.cur_index+1)+" of "+results.total+" results";
       })
   });
   }, false);
